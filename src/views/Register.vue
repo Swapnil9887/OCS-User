36195fd09586231337804d3bd102673572920296
Jon <jnation@lco.global>
2022-09-23 21:26:36 +0000

    fix formatting and match versions back

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,18 +1,17 @@
 <template>
-    <b-row>
-      <b-col>
-        <passthrough-form endpoint="/accounts/register/" />
-      </b-col>
-    </b-row>
-  </template>
-  <script>
-  import PassthroughForm from '@/components/PassthroughForm.vue';
+  <b-row>
+    <b-col>
+      <passthrough-form endpoint="/accounts/register/" />
+    </b-col>
+  </b-row>
+</template>
+<script>
+import PassthroughForm from '@/components/PassthroughForm.vue';
   
-  export default {
-    name: 'Register',
-    components: {
-      PassthroughForm
-    }
-  };
-  </script>
-  
+export default {
+  name: 'Register',
+  components: {
+    PassthroughForm
+  }
+};
+</script>
