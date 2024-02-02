a01cc6ad1ada01b818d764f4964737076ac80110
Jon <jnation@lco.global>
2024-02-02 20:49:16 -0100

    Fix missing AccountsGet view

---
+++ src/views/AccountsGet.vue
@@ -0,0 +1,28 @@
+<template>
+    <b-row>
+      <b-col>
+        <passthrough-get :endpoint="endpoint" :success-redirect-view-name="successRedirectViewName" />
+      </b-col>
+    </b-row>
+  </template>
+  <script>
+  import PassthroughGet from '@/components/PassthroughGet.vue';
+
+  export default {
+    name: 'AccountsPassthroughGetPage',
+    components: {
+      PassthroughGet
+    },
+    props: {
+      successRedirectViewName: {
+        type: String,
+        default: ''
+      }
+    },
+    data: function() {
+      return {
+        endpoint: this.$route.path
+      };
+    }
+  };
+  </script>
